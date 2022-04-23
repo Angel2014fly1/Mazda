2133b751e94d2b892b091a78ac022893c78bb425
Angel2014fly1 <101121237+Angel2014fly1@users.noreply.github.com>
2022-04-23 15:04:36 +0100

    Add files via upload

---
+++ Mazda.py
@@ -0,0 +1,225 @@
+from time import time
+import os
+import binascii
+import math
+import os.path
+
+lenf=0
+name=""
+add_bits=""
+Make_togher=""
+
+namez = input("Please, Mazda ")
+
+#@Author Jurijus Pacalovas
+class MazdaModel:
+       
+        def FindMazdaModel(self):
+                
+                self.name = "Written: Jurijus pacalovas"
+
+                if namez!="Mazda":
+                        print("The wrong letter")
+                        raise SystemExit
+                        
+                if namez=="Mazda":
+                        i=1
+
+                    
+                if i==1: 
+                    Number_add_plus_one=""
+                    Prime_Not=""
+                    Times_6=""
+                    Corrupted=0
+                      
+                    name = input("What is name of file? ")
+
+                    if os.path.exists(name):
+                            print('Path is exists!')
+                    else:
+                            print('Path is not exists!')
+                            raise SystemExit
+                            
+                    
+                    namem=""
+                    namema="?"
+        
+                    nameas=name
+                    nac=len(nameas)
+                    
+                    compress_or_not_compress=1
+                    Circle_times3=0
+                    
+                    if i==1:
+                        
+                        nameas=name+".bin"
+                    
+                    	
+                    nac=len(nameas)
+                    
+                   
+                    s=""
+
+                    Equal_info_between_of_the_cirlce_of_the_file=""
+                    Equal_info_between_of_the_cirlce_of_the_file_2=""
+
+                    Prime_Not=""
+                    Times_6=""
+
+                    Translate_info_Decimal=""
+
+                    D=0
+
+                    x=0
+                    x1=0
+                    x2=0
+                    n=0
+                    x = time()
+
+                    with open(nameas, "w") as f4:
+                            f4.write(s)
+                    with open(nameas, "a") as f3:
+                            f3.write(s)
+                    with open(name, "rb") as binary_file:
+
+                       # Read the whole file at once
+                        data = binary_file.read()
+      
+                        s=str(data)
+
+                        lenf1=len(data)
+                        lenf7=len(data)
+                        if lenf7==0:
+                        	 raise SystemExit
+                        
+                        END_working=0
+                        Circle_times2=0
+                                   
+                        Equal_info_between_of_the_cirlce_of_the_file_23=""
+ 
+                        sda18=""
+                        Equal_info_between_of_the_cirlce_of_the_file_29=""
+                        
+                        SpinS=0
+                        while END_working<10:
+                       
+                            Circle_times3=Circle_times3+1
+                            D=1
+                            if D==1:
+                                if Circle_times3==1:
+
+                                 
+                                    sda=bin(int(binascii.hexlify(data),16))[2:]
+                                    lenf=len(sda)
+                                    lenf1=len(data)
+                                
+                                    count_bits=(lenf1*8)-lenf
+                                    z=0
+                                    if count_bits!=0:
+                                        while z<count_bits:
+                                            sda="0"+sda
+                                            z=z+1
+                                            
+                                    
+
+                                    if Circle_times3==1:
+                                        Equal_info_between_of_the_cirlce_of_the_file_2=sda
+                            
+                                    n = int(Equal_info_between_of_the_cirlce_of_the_file_2, 2)
+                                
+                                    width_bits=len(Equal_info_between_of_the_cirlce_of_the_file_2)
+                                    width_bits=(width_bits/8)*2
+                                    width_bits=str(width_bits)
+                                    width_bits="%0"+width_bits+"x"
+                             
+                                    width_bits3=binascii.unhexlify(width_bits % n)                                    
+                                    width_bits2=len(width_bits3)
+                                    
+                                    data=width_bits3
+                                  
+                                    lenf5=len(data)
+
+                                    sda=bin(int(binascii.hexlify(data),16))[2:]
+                                    lenf=len(sda)
+
+                                    lenf1=len(data)
+                                
+                                    count_bits=(lenf1*8)-lenf
+                                    z=0
+                                    if count_bits!=0:
+                                        while z<count_bits:
+                                            sda="0"+sda
+                                            z=z+1
+
+                                    Equal_info_between_of_the_cirlce_of_the_file_2=sda
+
+                                    lenf3=len(Equal_info_between_of_the_cirlce_of_the_file_2)
+                                lenf2=len(Equal_info_between_of_the_cirlce_of_the_file_2)
+                                #print(lenf2)
+                                if i==1:
+                                    if lenf7>=(2**40)-1:
+                                        raise SystemExit
+
+                                #########################################################################################################################################################
+                                
+                                
+                                if i==1:
+
+                                    lenf5=len(Equal_info_between_of_the_cirlce_of_the_file_2)
+
+                                    Equal_info_between_of_the_cirlce_of_the_file=Equal_info_between_of_the_cirlce_of_the_file_2
+                                 
+                                    lenf5=len(Equal_info_between_of_the_cirlce_of_the_file)
+                                    
+                                    
+                                    #Extract
+                            
+                                    s=""
+
+                                    Equal_info_between_of_the_cirlce_of_the_file=Equal_info_between_of_the_cirlce_of_the_file_2
+                                    lenf6=len(Equal_info_between_of_the_cirlce_of_the_file)
+                                    
+                                    Number_add_plus_one=""
+                                    Prime_Not=""
+                                    Times_6=""
+                                  
+                                    Equal_info_between_of_the_cirlce_of_the_file_17=""
+                   
+                                    g=0
+
+                                    Equal_info_between_of_the_cirlce_of_the_file=Equal_info_between_of_the_cirlce_of_the_file_2
+
+                                    lenf6=len(Equal_info_between_of_the_cirlce_of_the_file)                      
+                                    Equal_info_between_of_the_cirlce_of_the_file_17=""
+                                
+                                    Equal_info_between_of_the_cirlce_of_the_file=Equal_info_between_of_the_cirlce_of_the_file_2
+                                    
+                                    lenf6=len(Equal_info_between_of_the_cirlce_of_the_file)
+                                
+                                    add_bits=""
+
+                                    Times_6=""
+
+                                    #Compression
+
+                                    sda10=""
+                                    Translate_info_Decimal=""
+                                    
+                                    Equal_info_between_of_the_cirlce_of_the_file_17=""
+                 
+                                    Equal_info_between_of_the_cirlce_of_the_file
+                                    ei=0
+                                    
+                                    Mazda1 = Equal_info_between_of_the_cirlce_of_the_file.find("111110001111100110010110")
+                                    print(Mazda1)
+                                    x2 = time()
+                                    x3=x2-x
+                                    xs=float(x3)
+                                    return print(x3)
+                                    
+                                    		
+                              
+d=MazdaModel()
+
+xw1=d.FindMazdaModel()
+print(xw1)
